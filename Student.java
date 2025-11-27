5ba16d34f813b496a17e3f1ab7e96413cf8207df
OmarMarg <omarmtzarg@gmail.com>
2025-11-27 16:41:10 -0600

    Se agregaron los métodos a Student

--- Student.java
+++ Student.java
@@ -5,4 +5,27 @@ public class Student {
     int registration;
     int grade;
     int year;
-}
+    
+    public void printFullName(String firstName, String lastName){
+        //TODO implement
+    	System.out.println(firstName + " " + lastName);
+     }
+
+     public void isApproved(int grade){
+         //TODO implement: should return true if grade >= 60
+    	 if (grade >= 60) {
+    		 year =+ 1;
+    		 System.out.println("Congrats, you approved the subject.");
+    		 
+    	 } else {
+    		 System.out.println("You didn't pass the subject.");
+    	 }
+     }
+
+     public int changeYearIfApproved(){
+         //TODO implement: the student should advance to the next year if he/she grade is >= 60
+         // Make year = year + 1, and print "Congragulations" if the student has been approved
+    	 return year;
+     }
+    
+}//Class Student
